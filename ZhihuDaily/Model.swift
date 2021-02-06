25b75a302b2fe5650ebcfeb3dbfb71c74a07e63d
车致远 <898065661@qq.com>
2021-02-06 21:54:29 +0800

    tryagain

--- ZhihuDaily/Model.swift
+++ ZhihuDaily/Model.swift
@@ -5,28 +5,169 @@
 //  Created by 车致远 on 2021/1/24.
 //
 
-import Foundation
 import Alamofire
-import SwiftyJSON
 import CoreData
+import Foundation
+import SwiftyJSON
 
 
-class Storie {
-    var title = String()
-    var ga_prefix = String()
-    var images = [String]()
-    var id = Int()
-}
+var stories = Story()
 
-class Top_Stories {
-    var title = String()
-    var ga_prefix = String()
-    var images = [String]()
-    var id = Int()
-}
-
-class test {
+/// 读取Coredata中的数据
+/// - Returns: 返回数组形式的Story
+func readStory() -> [Story] {
     let appDelegate: AppDelegate = UIApplication.shared.delegate as! AppDelegate
-    lazy var  managedObjectContext = appDelegate.persistentContainer.viewContext
+    let managedObjectContext = appDelegate.persistentContainer.viewContext
+    
+    
+    let entity: NSEntityDescription? =
+        NSEntityDescription.entity(forEntityName: "Story", in: managedObjectContext)
+    let request = NSFetchRequest<Story>(entityName: "Story")
+    var result: [AnyObject]?
+    request.fetchOffset = 0
+    request.entity = entity
+    result = try! managedObjectContext.fetch(request)
+    let temp = result as! [Story]
+    return temp
 }
+
+/// 获取当天的Story并存储至CoreData
+func writeLatestStory () {
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    let managedObjectContext = appDelegate.persistentContainer.viewContext
+    AF.request("https://news-at.zhihu.com/api/3/news/latest").responseJSON {
+        response in
+        switch response.result {
+        case .success(let json):
+            let data = JSON(json)
+            var date = data["date"].int32Value
+            var hasBeenAdded = false
+            for story in readStory() {
+                if story.date == date {
+                    hasBeenAdded = true
+                }
+            }
+            if !hasBeenAdded {
+                let stories = data["stories"].array!
+                for story in stories {
+                    let newStory = NSEntityDescription.insertNewObject(forEntityName: "Story", into: managedObjectContext) as! Story
+                    
+                    newStory.date = date
+                    newStory.hint = story["hint"].stringValue
+                    newStory.id = story["id"].int32Value
+                    newStory.title = story["title"].stringValue
+                    newStory.url = story["url"].stringValue
+                    if let images = story["images"].array {
+                        newStory.image = images[0].stringValue
+                    }
+                    
+                    do {
+                        try managedObjectContext.save()
+                    } catch {
+                        print("\(error)")
+                    }
+                }
+            }
+            
+            
+        case .failure(let json):
+            print(json.errorDescription)
+        }
+        NotificationCenter.default.post(name: Notification.Name(rawValue: "newstoryLoaded"), object: nil, userInfo: nil)
+
+    }
+    
+    do {
+        try managedObjectContext.save()
+    } catch {
+        print("\(error)")
+    }
+    
+}
+
+/// 获取先前的Story并存储至CoreData
+func writePreviousStory () { //
+   let appDelegate = UIApplication.shared.delegate as! AppDelegate
+   let managedObjectContext = appDelegate.persistentContainer.viewContext
+
+   var dates = [Int32]()
+   for storys in readStory() {
+       dates.append(storys.date)
+   }
+   var previousDate = String()
+   if let a = dates.min() {
+       previousDate = String(a)
+   } else {
+       print("storysDate is empty")
+   }
+   print("previousdate is \(previousDate)")
+   AF.request("https://news-at.zhihu.com/api/3/stories/before/\(previousDate)").responseJSON {
+       response in
+       switch response.result {
+       case .success(let json):
+           let data = JSON(json)
+           var date = data["date"].int32Value
+           var hasBeenAdded = false
+           for story in readStory() {
+               if story.date == date {
+                   hasBeenAdded = true
+               }
+           }
+           
+           if !hasBeenAdded {
+               let stories = data["stories"].array!
+               for story in stories {
+                   let newStory = NSEntityDescription.insertNewObject(forEntityName: "Story", into: managedObjectContext) as! Story
+                   newStory.date = date
+                   newStory.hint = story["hint"].stringValue
+                   newStory.id = story["id"].int32Value
+                   newStory.title = story["title"].stringValue
+                   newStory.url = story["url"].stringValue
+                   if let images = story["images"].array {
+                        newStory.image = images[0].stringValue
+                   }
+                   do {
+                       try managedObjectContext.save()
+                   } catch {
+                       print("\(error)")
+                   }
+               }
+           }
+       case .failure(let json):
+           print(json.errorDescription)
+       }
+       NotificationCenter.default.post(name: Notification.Name(rawValue: "newspaperLoaded"), object: nil, userInfo: nil)
+
+   }
+}
+
+
+func deleteAllStory() {
+    let appDelegate = UIApplication.shared.delegate as! AppDelegate
+    let managedObjectContext = appDelegate.persistentContainer.viewContext
+    let entity: NSEntityDescription? =
+        NSEntityDescription.entity(forEntityName: "Story", in: managedObjectContext)
+    let request = NSFetchRequest<Story>(entityName: "Story")
+    request.fetchOffset = 0
+    request.entity = entity
+    do{
+        let results:[AnyObject]? = try managedObjectContext.fetch(request)
+        for story: Story in results as! [Story]{
+            managedObjectContext.delete(story)
+        }
+        try managedObjectContext.save()
+        let _:[AnyObject]? = try managedObjectContext.fetch(request)
+    } catch {
+        let error = error as NSError
+        print(error)
+    }
+
+}
+
+
+
+
+
+
+
 
--- ZhihuDaily/ViewController.swift
+++ ZhihuDaily/ViewController.swift
@@ -12,14 +12,26 @@ import SwiftyJSON
 import CoreData
 
 class ViewController: UIViewController {
-    lazy var titleCollection = [String]()
+
     lazy var mainTableView: UITableView = {
         return UITableView()
-    
-    }()
+    }() //主界面的UITableView
     var container: NSPersistentContainer!
-    var pictureView = UIImageView()
-    var picture = UIImage(named: "pic1")
+    var keys = [Int32]()                //将date存入数组中 作为TableView划分section的依据
+    var stories = [Int32: [Story]]()   //对CoreData中的数据以date为key进行存储
+//    @objc func printsmth() {
+////        for story in readStorie() {
+////            print(story.date)
+////            print(story.hint)
+////            print(story.id)
+////            print(story.title)
+////            print(story.image)
+////        }
+//    }
+    
+    
+     
+    
     override func viewDidLoad() {
         
         self.view.addSubview(mainTableView)
@@ -32,57 +44,152 @@ class ViewController: UIViewController {
         mainTableView.delegate = self
         mainTableView.dataSource = self
         
-        view.addSubview(pictureView)
-        pictureView.snp.makeConstraints({ make -> Void in
-            make.size.width.height.equalTo(80)
-            make.center.equalToSuperview()
-        })
-        
-        pictureView.image = picture
-        
-        var image = picture!
-        
-        var documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
-        
-        documentsPath.appendPathComponent("filename1")
-        let data = image.jpegData(compressionQuality: 1.0)
-        FileManager.default.createFile(atPath: documentsPath.path, contents: data, attributes: nil)
-        
-        AF.request("https://news-at.zhihu.com/api/3/news/latest").responseJSON(completionHandler: {response in
-            switch response.result {
-            case .success(let json):
-                var url = String()
-                let data = JSON(json)
-                var  stories = data["stories"].array!
-                for store in stories {
-                    var title = store["title"].stringValue
-                    self.titleCollection.append(title)
-                    url = store["url"].stringValue
-                }
-                self.mainTableView.reloadData()
-                
-            print(url)
-            case .failure(let jsooo):
-                print(jsooo.errorDescription)
-            }
-        })
+        writeLatestStory()
+//        writePreviousStory()
+//        deleteAllStory()
+        storeImage()
 
+       
+        //将CoreData中date的值存入集合中，以获得无重复的日期值
+        var groups = Set<Int32>()
+        for story in readStory() {
+            groups.insert(story.date)
+        }
+        keys = groups.sorted(by:>)
+        //将CoreData中的元素按相同日期归至同一数组
+        readStory().forEach { story in
+            stories[story.date, default: []].append(story)
+        }
+        
+        
     }
     
     
 }
 
+
+    
+    
+
+
+
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return keys.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return titleCollection.count
+        return stories[keys[section], default: []].count
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        100
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "aaa"
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        var tableViewCell = UITableViewCell(style: .default, reuseIdentifier: nil)
-        print("current collection \(titleCollection)")
-        tableViewCell.textLabel?.text = titleCollection[indexPath.row]
-        return tableViewCell
+        let identifer = "reusedCell"
+        var cell = tableView.dequeueReusableCell(withIdentifier: identifer)
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: identifer)
+        }
+        
+        let story = stories[keys[indexPath.section], default: []][indexPath.row]
+        
+        cell?.textLabel?.text = story.title
+        cell?.textLabel?.numberOfLines = 2
+        cell?.detailTextLabel?.text = story.hint
+        cell?.detailTextLabel?.textColor = UIColor.gray
+        
+        let imageView = UIImageView(image: readImage(of: story.id))
+        imageView.frame.size = CGSize(width: 70, height: 70)
+        imageView.contentMode = .scaleAspectFit
+        imageView.makeViewRoundCorner()
+        cell?.accessoryView = imageView
+        
+        return cell!
+    }
+
+}
+
+
+extension UIImageView {
+    
+    
+    /// 下载图片
+    /// - Parameter url: URL
+    func downloadedFrom(url: URL) {
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard
+                let httpURLResponse = response as? HTTPURLResponse, httpURLResponse.statusCode == 200,
+                let mimeType = response?.mimeType, mimeType.hasPrefix("image"),
+                let data = data, error == nil,
+                let image = UIImage(data: data)
+                else { return }
+            DispatchQueue.main.async() {
+                self.image = image
+            }
+            }.resume()
     }
     
     
+    /// 下载图片
+    /// - Parameters:
+    ///   - link: 网络链接
+    func downloadedFrom(link: String) {
+        guard let url = URL(string: link) else { return }
+        downloadedFrom(url: url)
+    }
+    
+}
+
+extension ViewController {
+    
+    /// 将图片以Story的id.jpg为文件名进行存储
+    func storeImage() {
+        for story in readStory() {
+            var imageView = UIImageView()
+            if let link = story.image {
+                imageView.downloadedFrom(link: "\(link)")
+            }else {
+                print("imageUrl in storie is empty")
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                let image = imageView.image
+                var documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+                documentsPath.appendPathComponent("\(story.id).jpg")
+                let data = image!.jpegData(compressionQuality: 1.0)
+                FileManager.default.createFile(atPath: documentsPath.path, contents: data, attributes: nil)
+            }
+           
+           
+            
+        }
+    }
+    
+    
+    /// 读取某一篇文章对应的头图
+    /// - Parameter id: 文章的ID
+    /// - Returns: 文章对应的头图
+    func readImage(of id: Int32) -> UIImage? {
+        var documentsPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
+        documentsPath.appendPathComponent("\(id).jpg")
+        return UIImage(contentsOfFile: documentsPath.path)
+    }
+}
+
+extension UIView {
+    
+    /// 将UIView进行圆角处理
+    func makeViewRoundCorner() {
+        layer.cornerRadius = frame.width / 32
+        layer.masksToBounds = true
+    }
 }
